0dcd0d597701f9bd6bba71754cbbe094162ae1ff
Raenn <danbelsey@hotmail.co.uk>
2022-07-23 19:32:00 +0100

    remove winston, attempting to work with old node versions

--- bot.js
+++ bot.js
@@ -1,15 +1,7 @@
 var Discord = require('discord.io');
 
-var logger = require('winston');
 var token = require("./auth.json").token;
 const permissionsInteger = 1073810432;
-
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-	colorize: true
-});
-
-logger.level = 'debug';
 
 // Initialize Discord Bot
 var bot = new Discord.Client({
@@ -18,9 +10,7 @@ var bot = new Discord.Client({
 });
 
 bot.on('ready', function (evt) {
-	logger.info('Connected');
-	logger.info('Logged in as: ');
-	logger.info(bot.username + ' - (' + bot.id + ')');
+	console.log("started!")
 });
 
 bot.on('message', function (user, userID, channelID, message, evt) {
@@ -68,8 +58,8 @@ if (message.substring(0, 1) == '!') {
 						});
 				} else {
 					var message = ""
-					for (let i = 0; i < diceCount; i++) {
-						let index = Math.floor(Math.random() * 6)
+					for (var i = 0; i < diceCount; i++) {
+						var index = Math.floor(Math.random() * 6)
 						message += emoji[index]
 					}
 
